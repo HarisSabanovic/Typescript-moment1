d205deff085fe4becc3a81a031f64e4677ef2dcb
Haris Sabanovic <hasa2303@student.miun.se>
2024-03-26 14:32:52 +0100

    de listar ut det användaren har skrivit

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,25 @@
-//variabler
+//knapp variabler
 var saveBtnEl = document.getElementById("saveBtn");
 var deleteBtnEl = document.getElementById("deleteBtn");
+//input variabler 
+var courseCode = document.getElementById("code");
+var courseName = document.getElementById("name");
+var courseLink = document.getElementById("link");
+var courseProg = document.getElementById("progression");
+console.log(courseCode, courseLink, courseName, courseProg);
+//div variabel
+var courseListEl = document.getElementById("courseList");
+//eventlyssnare o funktion
+saveBtnEl.addEventListener("click", function () {
+    var codeInput = courseCode.value;
+    var nameInput = courseName.value;
+    var linkInput = courseLink.value;
+    var progInput = courseProg.value;
+    if (codeInput === "" || nameInput === "" || linkInput === "" || progInput === "") {
+        alert("Fyll i alla fält");
+    }
+    else {
+        courseListEl.innerHTML =
+            "<h3>".concat(nameInput, "</h3>\n        <p>Kurskod: ").concat(codeInput, "</p>\n        <p>Kursl\u00E4nk: ").concat(linkInput, "</p>\n        <p>Progression: ").concat(progInput, "</p>\n        ");
+    }
+});
--- src/js/main.ts
+++ src/js/main.ts
@@ -4,10 +4,57 @@ interface courseInfo {
     code: string;
     name: string;
     progression: string;
-    syllabus: string;
+    link: string;
 }
 
-//variabler
+//knapp variabler
 let saveBtnEl = document.getElementById("saveBtn") as HTMLInputElement;
 let deleteBtnEl = document.getElementById("deleteBtn") as HTMLInputElement;
 
+//input variabler 
+let courseCode = document.getElementById("code") as HTMLInputElement;
+let courseName = document.getElementById("name") as HTMLInputElement;
+let courseLink = document.getElementById("link") as HTMLInputElement;
+let courseProg = document.getElementById("progression") as HTMLInputElement;
+
+console.log(courseCode, courseLink, courseName, courseProg);
+
+//div variabel
+let courseListEl = document.getElementById("courseList") as HTMLDivElement;
+
+//eventlyssnare o funktion
+saveBtnEl.addEventListener("click", () => {
+
+    const codeInput: string = courseCode.value;
+    const nameInput: string = courseName.value;
+    const linkInput: string = courseLink.value;
+    const progInput: string = courseProg.value;
+
+    const newInfo : courseInfo = {
+        code: codeInput,
+        name: nameInput,
+        link: linkInput,
+        progression: progInput
+    };
+
+    if(codeInput === "" || nameInput === "" || linkInput === "" || progInput === "") {
+        alert("Fyll i alla fält");
+    } else {
+        courseListEl.innerHTML = 
+        `<h3>${nameInput}</h3>
+        <p>Kurskod: ${codeInput}</p>
+        <p>Kurslänk: ${linkInput}</p>
+        <p>Progression: ${progInput}</p>
+        `
+    }
+
+    localStorage.setItem("newInfo", JSON.stringify(newInfo));
+});
+
+window.onload = () => {
+    let storedCourse = localStorage.getItem("newinfo");
+
+    if (storedCourse) {
+        
+    }
+}
